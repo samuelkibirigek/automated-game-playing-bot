2ae1132dec2a2b69852c063d1df595d605bd7cb3
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-02-21 20:37:35 +0300

    Some corrections to the cookie clicker game bot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 
 
@@ -17,28 +16,33 @@ upgrade_check = 5
 
 
 while True:
-    cursor_price = int(driver.find_element(By.CSS_SELECTOR, "#buyCursor b").text.split()[2])
-    grandma_price = int(driver.find_element(By.CSS_SELECTOR, "#buyGrandma b").text.split()[2])
-    factory_price = int(driver.find_element(By.CSS_SELECTOR, "#buyFactory b").text.split()[2])
-    mine_price = int(driver.find_element(By.CSS_SELECTOR, "#buyMine b").text.split()[2].replace(",", ""))
-    shipment_price = int(driver.find_element(By.CSS_SELECTOR, "#buyShipment b").text.split()[2].replace(",", ""))
-    alchemy_price = int(driver.find_element(By.XPATH, '//*[@id="buyAlchemy lab"]/b').text.split()[3].replace(",", ""))
-    portal_price = int(driver.find_element(By.CSS_SELECTOR, "#buyPortal b").text.split()[2].replace(",", ""))
-    timeMachine_price = int(driver.find_element(By.XPATH, '//*[@id="buyTime machine"]/b').text.split()[3].replace(",", ""))
 
     cookie.click()
     if time.time() > upgrade_check:
-        money = int(driver.find_element(By.ID, "money").text)
-        if money > 15 and cursor_price < 25:
+        cursor_price = int(driver.find_element(By.CSS_SELECTOR, "#buyCursor b").text.split()[2])
+        grandma_price = int(driver.find_element(By.CSS_SELECTOR, "#buyGrandma b").text.split()[2])
+        factory_price = int(driver.find_element(By.CSS_SELECTOR, "#buyFactory b").text.split()[2])
+        mine_price = int(driver.find_element(By.CSS_SELECTOR, "#buyMine b").text.split()[2].replace(",", ""))
+        shipment_price = int(driver.find_element(By.CSS_SELECTOR, "#buyShipment b").text.split()[2].replace(",", ""))
+        alchemy_price = int(
+            driver.find_element(By.XPATH, '//*[@id="buyAlchemy lab"]/b').text.split()[3].replace(",", ""))
+        portal_price = int(driver.find_element(By.CSS_SELECTOR, "#buyPortal b").text.split()[2].replace(",", ""))
+        timeMachine_price = int(
+            driver.find_element(By.XPATH, '//*[@id="buyTime machine"]/b').text.split()[3].replace(",", ""))
+        try:
+            money = int(driver.find_element(By.ID, "money").text)
+        except ValueError:
+            money = int(driver.find_element(By.ID, "money").text.replace(",", ""))
+        if cursor_price < 25 and money > cursor_price:
             cursor = driver.find_element(By.ID, "buyCursor")
             cursor.click()
         elif money > 111 and grandma_price < 150:
             cursor = driver.find_element(By.ID, "buyGrandma")
             cursor.click()
-        elif money > 500 and factory_price < 600:
+        elif money > 500 and factory_price < 750:
             cursor = driver.find_element(By.ID, "buyFactory")
             cursor.click()
-        elif money > 2000:
+        elif money > 2000 and mine_price < 2700:
             cursor = driver.find_element(By.ID, "buyMine")
             cursor.click()
         elif money > 7000:
@@ -55,7 +59,6 @@ while True:
             cursor.click()
         else:
             continue
-
 
     if time.time() > timeout:
         cookies_per_second = driver.find_element(By.ID, "cps").text
